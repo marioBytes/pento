5d6ecf831d7ef20cc86b1834daec547c991a141f
marioBytes <mario.bytes92@gmail.com>
2022-06-23 15:02:08 -0600

    Add helper functions to create changeset

--- lib/pento_web/live/promo_live.ex
+++ lib/pento_web/live/promo_live.ex
@@ -4,6 +4,17 @@ defmodule PentoWeb.PromoLive do
   alias Pento.Promo.Recipient
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+     socket
+     |> assign_recipient()
+     |> assign_changeset()}
+  end
+
+  def assign_recipient(socket) do
+    socket |> assign(:recipient, %Recipient{})
+  end
+
+  def assign_changeset(%{assigns: %{recipient: recipient}} = socket) do
+    socket |> assign(:changeset, Promo.change_recipient(recipient))
   end
 end
