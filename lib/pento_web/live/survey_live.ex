87d5561b7e6c589a730c1d719bb67884bf337c55
marioBytes <mario.bytes92@gmail.com>
2022-06-27 10:41:05 -0600

    Use component to display user demographic info

--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -3,7 +3,14 @@ defmodule PentoWeb.SurveyLive do
 
   alias __MODULE__.Component
 
+  alias Pento.Survey
+  alias PentoWeb.DemographicLive
+
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, socket |> assign_demographic()}
+  end
+
+  defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket, :demographic, Survey.get_demographic_by_user(current_user))
   end
 end
