f18ec2ca44160d55037f174242dd47b2bfa14d0f
marioBytes <mario.bytes92@gmail.com>
2022-06-27 10:12:52 -0600

    Create component to show demographic input

---
+++ lib/pento_web/live/demographic_live/show.ex
@@ -0,0 +1,16 @@
+defmodule PentoWeb.DemographicLive.Show do
+  use Phoenix.Component
+  use Phoenix.HTML
+
+  def details(assigns) do
+    ~H"""
+    <div>
+      <h2>Demographics <%= raw "&#x2713;" %></h2>
+      <ul>
+        <li>Gender: <%= @demographic.gender %></li>
+        <li>Year of birth: <%= @demographic.year_of_birth %></li>
+      </ul>
+    </div>
+    """
+  end
+end
