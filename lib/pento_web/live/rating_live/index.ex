1d97ecd4d5b33985340f18ffbe7a4aecda8d5fd7
marioBytes <mario.bytes92@gmail.com>
2022-06-29 10:17:15 -0600

    Create components to render product ratings

---
+++ lib/pento_web/live/rating_live/index.ex
@@ -0,0 +1,46 @@
+defmodule PentoWeb.RatingLive.Index do
+  use Phoenix.Component
+  use Phoenix.HTML
+
+  alias PentoWeb.RatingLive
+
+  def products(assigns) do
+    ~H"""
+      <div class="survey-component-container">
+        <.heading products={@products} />
+        <.list products={@products} />
+      </div>
+    """
+  end
+
+  def heading(assigns) do
+    ~H"""
+      <h2>
+        Ratings
+        <%= if ratings_complete?(@products), do: raw "&#x2713;" %>
+      </h2>
+    """
+  end
+
+  def list(assigns) do
+    ~H"""
+      <%= for {product, index} <- Enum.with_index(@products) do %>
+        <%= if rating = List.first(product.ratings) do %>
+          <RatingLive.Show.stars rating={rating} product={product} />
+        <% else %>
+          <.live_component
+            module={RatingLive.Form}
+            id={"rating-form-#{product.id}"}
+            product={product}
+            product_index={index}
+            current_user={@current_user}
+          />
+        <% end %>
+      <% end %>
+    """
+  end
+
+  defp ratings_complete?(products) do
+    Enum.all?(products, fn product -> not Enum.empty?(product) end)
+  end
+end
