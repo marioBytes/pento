18b58af5b7f66bb5f7acfdc09ded87ecd3ec4c8e
marioBytes <mario.bytes92@gmail.com>
2022-06-29 11:23:28 -0600

    Handle rating form events

--- lib/pento_web/live/rating_live/form.ex
+++ lib/pento_web/live/rating_live/form.ex
@@ -12,11 +12,42 @@ defmodule PentoWeb.RatingLive.Form do
      |> assign_changeset}
   end
 
+  def handle_event("validate", %{"rating" => rating_params}, socket) do
+    {:noreply, validate_rating(socket, rating_params)}
+  end
+
+  def handle_event("save", %{"rating" => rating_params}, socket) do
+    {:noreply, save_rating(socket, rating_params)}
+  end
+
   def assign_rating(%{assigns: %{current_user: user, product: product}} = socket) do
     assign(socket, :rating, %Rating{user_id: user.id, product_id: product.id})
   end
 
   def assign_changeset(%{assigns: %{rating: rating}} = socket) do
     assign(socket, :changeset, Survey.change_rating(rating))
+  end
+
+  defp validate_rating(socket, rating_params) do
+    changeset =
+      socket.assigns.rating
+      |> Survey.change_rating(rating_params)
+      |> Map.put(:action, :validate)
+
+    assign(socket, :changeset, changeset)
+  end
+
+  defp save_rating(
+         %{assigns: %{product_index: product_index, product: product}} = socket,
+         rating_params
+       ) do
+    case Survey.create_rating(rating_params) do
+      {:ok, rating} ->
+        product = %{product | ratings: [rating]}
+        send(self(), {:created_rating, product, product_index})
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        assign(socket, changeset: changeset)
+    end
   end
 end
