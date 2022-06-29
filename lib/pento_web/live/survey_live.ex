1e612ec024293b2465365a5708f9e7e9f1ee0196
marioBytes <mario.bytes92@gmail.com>
2022-06-29 11:30:33 -0600

    Handle component event when rating submitted

--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -24,6 +24,10 @@ defmodule PentoWeb.SurveyLive do
     {:noreply, handle_demographic_created(socket, demographic)}
   end
 
+  def handle_info({:created_rating, updated_product, product_index}, socket) do
+    {:noreply, handle_rating_created(socket, updated_product, product_index)}
+  end
+
   defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
     assign(socket, :demographic, Survey.get_demographic_by_user(current_user))
   end
@@ -36,6 +40,16 @@ defmodule PentoWeb.SurveyLive do
     socket
     |> put_flash(:info, "Demographic info has been saved")
     |> assign(:demographic, demographic)
+  end
+
+  defp handle_rating_created(
+         %{assigns: %{products: products}} = socket,
+         updated_product,
+         product_index
+       ) do
+    socket
+    |> put_flash(:info, "Rating submitted successfully")
+    |> assign(:products, List.replace_at(products, product_index, updated_product))
   end
 
   defp list_products(user) do
