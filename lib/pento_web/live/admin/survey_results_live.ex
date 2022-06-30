c18830c62096567ccac904d2bd42445e38dffca6
marioBytes <mario.bytes92@gmail.com>
2022-06-30 17:31:40 -0600

    Initialize Contex dataset to create chart

--- lib/pento_web/live/admin/survey_results_live.ex
+++ lib/pento_web/live/admin/survey_results_live.ex
@@ -4,10 +4,33 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
   alias Pento.Catalog
 
   def update(assigns, socket) do
-    {:ok, socket |> assign(assigns) |> assign_products_with_average_ratings()}
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign_products_with_average_ratings()
+     |> assign_dataset()
+     |> assign_chart()}
   end
 
   defp assign_products_with_average_ratings(socket) do
     socket |> assign(:products_with_average_ratings, Catalog.products_with_average_ratings())
+  end
+
+  def assign_dataset(
+        %{assigns: %{products_with_average_ratings: products_with_average_ratings}} = socket
+      ) do
+    socket |> assign(:dataset, make_bar_chart_dataset(products_with_average_ratings))
+  end
+
+  defp make_bar_chart_dataset(data) do
+    Contex.Dataset.new(data)
+  end
+
+  defp assign_chart(%{assigns: %{dataset: dataset}} = socket) do
+    socket |> assign(:chart, make_bar_chart(dataset))
+  end
+
+  defp make_bar_chart(dataset) do
+    Contex.BarChart.new(dataset)
   end
 end
