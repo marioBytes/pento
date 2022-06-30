3fc62bb95cebd07db8bdcdb580df34841c2d8b86
marioBytes <mario.bytes92@gmail.com>
2022-06-30 17:09:52 -0600

    Add product avg ratings to socket

--- lib/pento_web/live/admin/survey_results_live.ex
+++ lib/pento_web/live/admin/survey_results_live.ex
@@ -1,5 +1,13 @@
 defmodule PentoWeb.Admin.SurveyResultsLive do
   use PentoWeb, :live_component
 
+  alias Pento.Catalog
 
+  def update(assigns, socket) do
+    {:ok, socket |> assign(assigns) |> assign_products_with_average_ratings()}
+  end
+
+  defp assign_products_with_average_ratings(socket) do
+    socket |> assign(:products_with_average_ratings, Catalog.products_with_average_ratings())
+  end
 end
