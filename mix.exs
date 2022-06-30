747d4c680eb7e01430faa5932fa07b8883b43ce4
marioBytes <mario.bytes92@gmail.com>
2022-06-30 17:28:49 -0600

    Install Contex for charting

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Pento.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 2.0"},
+      {:contex, "~> 0.4.0"},
       {:phoenix, "~> 1.6.5"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
