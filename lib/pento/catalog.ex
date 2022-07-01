09cfb36d66d8bafdcd9d255564c26294feb6a309
marioBytes <mario.bytes92@gmail.com>
2022-07-01 09:42:16 -0600

    Build queries to filter by demographics

--- lib/pento/catalog.ex
+++ lib/pento/catalog.ex
@@ -9,8 +9,11 @@ defmodule Pento.Catalog do
     |> Repo.all()
   end
 
-  def products_with_average_ratings() do
+  def products_with_average_ratings(%{age_group_filter: age_group_filter}) do
     Product.Query.with_average_ratings()
+    |> Product.Query.join_users()
+    |> Product.Query.join_demographics()
+    |> Product.Query.filter_by_age_group(age_group_filter)
     |> Repo.all()
   end
 
--- lib/pento/catalog/product/query.ex
+++ lib/pento/catalog/product/query.ex
@@ -2,6 +2,8 @@ defmodule Pento.Catalog.Product.Query do
   import Ecto.Query
   alias Pento.Catalog.Product
   alias Pento.Survey.Rating
+  alias Pento.Accounts.User
+  alias Pento.Survey.Demographic
 
   def base() do
     Product
@@ -34,4 +36,52 @@ defmodule Pento.Catalog.Product.Query do
     |> group_by([p], p.id)
     |> select([p, r], {p.name, fragment("?::float", avg(r.stars))})
   end
+
+  def join_users(query \\ base()) do
+    query |> join(:left, [p, r], u in User, on: r.user_id == u.id)
+  end
+
+  def join_demographics(query \\ base()) do
+    query |> join(:left, [p, r, u, d], d in Demographic, on: d.user_id == u.id)
+  end
+
+  def filter_by_age_group(query \\ base(), filter) do
+    query |> apply_age_group_filter(filter)
+  end
+
+  defp apply_age_group_filter(query, "18 and under") do
+    birth_year = DateTime.utc_now().year - 18
+
+    query |> where([p, r, u, d], d.year_of_birth >= ^birth_year)
+  end
+
+  defp apply_age_group_filter(query, "18 to 25") do
+    birth_year_max = DateTime.utc_now().year - 18
+    birth_year_min = DateTime.utc_now().year - 25
+
+    query
+    |> where(
+      [p, r, u, d],
+      d.year_of_birth >= ^birth_year_min and d.year_of_birth <= ^birth_year_max
+    )
+  end
+
+  defp apply_age_group_filter(query, "25 to 35") do
+    birth_year_max = DateTime.utc_now().year - 25
+    birth_year_min = DateTime.utc_now().year - 35
+
+    query
+    |> where(
+      [p, r, u, d],
+      d.year_of_birth >= ^birth_year_min and d.year_of_birth <= ^birth_year_max
+    )
+  end
+
+  defp apply_age_group_filter(query, "35 and up") do
+    birth_year = DateTime.utc_now().year - 35
+
+    query |> where([p, r, u, d], d.year_of_birth <= ^birth_year)
+  end
+
+  defp apply_age_group_filter(query, _filter), do: query
 end
