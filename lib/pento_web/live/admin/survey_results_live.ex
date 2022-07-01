0ed26b0c8769539c9facf2f59e204ed879e9d3e7
marioBytes <mario.bytes92@gmail.com>
2022-07-01 10:00:32 -0600

    Add the age group filter to component state

--- lib/pento_web/live/admin/survey_results_live.ex
+++ lib/pento_web/live/admin/survey_results_live.ex
@@ -8,14 +8,25 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
     {:ok,
      socket
      |> assign(assigns)
+     |> assign_age_group_filter()
      |> assign_products_with_average_ratings()
      |> assign_dataset()
      |> assign_chart()
      |> assign_chart_svg()}
   end
 
-  defp assign_products_with_average_ratings(socket) do
-    socket |> assign(:products_with_average_ratings, Catalog.products_with_average_ratings())
+  def assign_age_group_filter(socket) do
+    socket |> assign(:age_group_filter, "all")
+  end
+
+  defp assign_products_with_average_ratings(
+         %{assigns: %{age_group_filter: age_group_filter}} = socket
+       ) do
+    assign(
+      socket,
+      :products_with_average_ratings,
+      Catalog.products_with_average_ratings(%{age_group_filter: age_group_filter})
+    )
   end
 
   def assign_dataset(
