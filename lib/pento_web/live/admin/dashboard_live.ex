3e53837499f05649124ec6049054d66394df1452
marioBytes <mario.bytes92@gmail.com>
2022-07-02 15:46:37 -0600

    Subscribe to survey results messages

--- lib/pento_web/live/admin/dashboard_live.ex
+++ lib/pento_web/live/admin/dashboard_live.ex
@@ -1,7 +1,20 @@
 defmodule PentoWeb.Admin.DashboardLive do
   use PentoWeb, :live_view
 
+  alias PentoWeb.Endpoint
+
+  @survey_results_topic "survey_results"
+
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      Endpoint.subscribe(@survey_results_topic)
+    end
+
     {:ok, socket |> assign(:survey_results_component_id, "survey-results")}
+  end
+
+  def handle_info(%{event: "rating_created"}, socket) do
+    send_update(PentoWeb.Admin.SurveyResultsLive, id: socket.assigns.survey_results_component_id)
+    {:noreply, socket}
   end
 end
