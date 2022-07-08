bfd43e9a37ca2bf0ff68a036ae086fc2986faf8b
marioBytes <mario.bytes92@gmail.com>
2022-07-08 14:06:58 -0600

    Define the board constructor

--- lib/pento/game/board.ex
+++ lib/pento/game/board.ex
@@ -9,4 +9,14 @@ defmodule Pento.Game.Board do
   ]
 
   def puzzles(), do: ~w[default wide widest medium tiny]a
+
+  def new(palette, points) do
+    %__MODULE__{palette: palette(palette), points: points}
+  end
+
+  def new(:tiny), do: new(:small, rect(5, 3))
+  def new(:widest), do: new(:all, rect(20, 3))
+  def new(:wide), do: new(:all, rect(15, 4))
+  def new(:medium), do: new(:all, rect(12, 5))
+  def new(:default), do: new(:all, rect(10, 6))
 end
