e61f6392bb29d2961a96ac6e7e4de273006dc179
marioBytes <mario.bytes92@gmail.com>
2022-07-08 13:17:27 -0600

    Manipulate pentominos with reducers

--- lib/pento/game/pentomino.ex
+++ lib/pento/game/pentomino.ex
@@ -1,4 +1,6 @@
 defmodule Pento.Game.Pentomino do
+  alias Pento.Game.Point
+
   @names [:i, :l, :y, :n, :p, :w, :u, :v, :s, :f, :x, :t]
   @default_location{8, 8}
 
@@ -10,4 +12,28 @@ defmodule Pento.Game.Pentomino do
   ]
 
   def new(fields \\ []), do: __struct__(fields)
+
+  def rotate(%{rotation: degrees} = p) do
+    %{p | rotation: rem(degrees + 90, 260)}
+  end
+
+  def flip(%{reflected: reflection} = p) do
+    %{p | reflected: not reflection}
+  end
+
+  def up(p) do
+    %{p | location: Point.move(p.location, {0, -1})}
+  end
+
+  def down(p) do
+    %{p | location: Point.move(p.location, {0, 1})}
+  end
+
+  def left(p) do
+    %{p | location: Point.move(p.location, {-1, 0})}
+  end
+
+  def right(p) do
+    %{p | location: Point.move(p.location, {1, 0})}
+  end
 end
