7e1205cffa31c3f4da9d11d580e68fbe8b191d93
marioBytes <mario.bytes92@gmail.com>
2022-07-08 13:10:07 -0600

    Represent pentomino attrs

---
+++ lib/pento/game/pentomino.ex
@@ -0,0 +1,11 @@
+defmodule Pento.Game.Pentomino do
+  @names [:i, :l, :y, :n, :p, :w, :u, :v, :s, :f, :x, :t]
+  @default_location{8, 8}
+
+  defstruct [
+    name: :i,
+    rotation: 0,
+    reflected: false,
+    location: @default_location
+  ]
+end
