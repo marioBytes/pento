16d6d862021afe5347fefd444ce062adcdbd3c71
marioBytes <mario.bytes92@gmail.com>
2022-07-08 10:56:34 -0600

    Move points geometrically with reducers

--- lib/pento/game/point.ex
+++ lib/pento/game/point.ex
@@ -6,4 +6,10 @@ defmodule Pento.Game.Point do
   def move({x, y}, {change_x, change_y}) do
     {x + change_x, y + change_y}
   end
+
+  def transpose({x, y}), do: {y, x}
+
+  def flip({x, y}), do: {x, 6 - y}
+
+  def reflect({x, y}), do: {6 - x, y}
 end
